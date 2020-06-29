2ec8a64708fb442e98c5d9219e66c96e36b8bcc7
Josh Moore <jxm5210@rit.edu>
2020-06-29 14:46:41 -0400

    Set backgrounds based on ID

--- xwow.c
+++ xwow.c
@@ -2,6 +2,8 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <signal.h>
 
 #include "xwow.h"
 
@@ -15,8 +17,39 @@ SDL_Renderer* sdl_rend;
 // this is called every frame
 void (*xwow_render)(SDL_Renderer*);
 
-int main()
+// index in the render func array
+int bg_idx = 0;
+
+void cleanup(int d)
 {
+    SDL_DestroyRenderer(sdl_rend);
+    SDL_DestroyWindow(sdl_win);
+    SDL_Quit();
+
+    XCloseDisplay(x_disp);
+
+    exit(0);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc > 2)
+    {
+        fprintf(stderr, "Too many arguments.\n");
+        fprintf(stderr, "Usage: xwow [id]");
+    }
+    else if (argc == 2)
+    {
+        // user provided a background id
+        bg_idx = atoi(argv[1]);
+    }
+    else
+    {
+        // user did not provide an id, pick random background
+        srand(time(NULL));
+        bg_idx = rand() % BG_COUNT;
+    }
+
     // initialize X
     if (!(x_disp = XOpenDisplay(NULL)))
     {
@@ -31,44 +64,30 @@ int main()
         fprintf(stderr, "xwow: failed to init SDL\n");
         exit(1);
     }
+
     if (!(sdl_win = SDL_CreateWindowFrom((void *)x_root)))
     {
         fprintf(stderr, "xwow: failed to create SDL window\n");
         exit(1);
     }
 
-    // lastly, get our SDL renderer
+    // get our SDL renderer
     if (!(sdl_rend = SDL_CreateRenderer(sdl_win, -1, 0)))
     {
         fprintf(stderr, "xwow: failed to create SDL renderer\n");
         exit(1);
     }
 
-    xwow_render = &bg_cyber_draw;
+    signal(SIGINT, cleanup);
 
-    SDL_Event evt;
+    xwow_render = BG_RENDER_FUNCS[bg_idx];
 
     while (1)
     {
         SDL_Delay(10);
-        SDL_PollEvent(&evt);
-
-        switch (evt.type)
-        {
-            case SDL_QUIT:
-                goto cleanup;
-                break;
-        }
+        SDL_PollEvent(NULL);
 
         // draw the background
         xwow_render(sdl_rend);
     }
-
-    cleanup:
-        // TODO: exit hotkey
-        SDL_DestroyRenderer(sdl_rend);
-        SDL_DestroyWindow(sdl_win);
-        SDL_Quit();
-
-        XCloseDisplay(x_disp);
 }
--- xwow.h
+++ xwow.h
@@ -7,8 +7,21 @@
 #ifndef XWOW_H
 #define XWOW_H
 
+#include <stdlib.h>
+
 #include "bg/bg_corners.h"
 #include "bg/bg_fade.h"
 #include "bg/bg_cyber.h"
+
+// main array of pointers to background draw functions
+const void* BG_RENDER_FUNCS[] =
+{
+    &bg_corners_draw,
+    &bg_fade_draw,
+    &bg_cyber_draw
+};
+
+// total number of backgrounds in the array
+const int BG_COUNT = sizeof(BG_RENDER_FUNCS) / sizeof(void*);
 
 #endif
